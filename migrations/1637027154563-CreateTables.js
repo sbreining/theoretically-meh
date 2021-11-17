87c4cb32c3ed5b84b9b560fb84940f5010189220
sbreining <sfbreining@gmail.com>
2021-11-16 17:02:15 -0700

    Change Tabs to Spaces

--- migrations/1637027154563-CreateTables.js
+++ migrations/1637027154563-CreateTables.js
@@ -5,7 +5,7 @@ module.exports = class CreateTables1637027154563 {
     async up(queryRunner) {
         await queryRunner.query(`
             CREATE TABLE \`viewers\` (
-	    	\`id\` INT unsigned NOT NULL AUTO_INCREMENT,
+                \`id\` INT unsigned NOT NULL AUTO_INCREMENT,
                 \`name\` VARCHAR(25) NOT NULL,
                 \`points\` INT unsigned NOT NULL DEFAULT '0',
                 \`created_at\` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -15,7 +15,7 @@ module.exports = class CreateTables1637027154563 {
 
         await queryRunner.query(`
             CREATE TABLE \`tokens\` (
-	    	\`id\` INT unsigned NOT NULL AUTO_INCREMENT,
+                \`id\` INT unsigned NOT NULL AUTO_INCREMENT,
                 \`service\` VARCHAR(256) NOT NULL DEFAULT 'TWITCH',
                 \`token\` VARCHAR(256) NOT NULL DEFAULT '0',
                 \`expiration\` INT unsigned NOT NULL,
