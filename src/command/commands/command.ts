525dfd5529087ff1455c7850168935dd76700382
KettelBear <kttl_crn@hotmail.com>
2023-02-03 16:00:46 -0700

    Make Use of Interface Still

--- src/command/commands/command.ts
+++ src/command/commands/command.ts
@@ -5,12 +5,23 @@ export type CommandArgs = {
   context?: ChatUserstate;
 };
 
+interface Command {
+  /**
+   * This shall serve as the execution when a command
+   * is entered in chat.
+   *
+   * @param {CommandArgs} args - If any are necessary
+   * @returns {string|Promise<string>} - The string response.
+   */
+  exec(args?: CommandArgs): string | Promise<string>;
+}
+
 /**
  * This will stand as the definition for commands.
  * All commands within the `commands/` folder should
  * implement this interface.
  */
-export abstract class UserCommand {
+export abstract class UserCommand implements Command {
   public readonly IS_MOD_CMD = false;
 
   // Name of the command that will be sent from chat
@@ -19,25 +30,11 @@ export abstract class UserCommand {
   // How to use the command
   abstract instruction: string;
 
-  /**
-   * This shall serve as the execution when a command
-   * is entered in chat.
-   *
-   * @param {CommandArgs} args - If any are necessary
-   * @returns {string|Promise<string>} - The string response.
-   */
   abstract exec(args?: CommandArgs): string | Promise<string>;
 }
 
-export abstract class ModCommand {
+export abstract class ModCommand implements Command {
   public readonly IS_MOD_CMD = true;
 
-  /**
-   * This shall serve as the execution when a command
-   * is entered in chat.
-   *
-   * @param {CommandArgs} args - If any are necessary
-   * @returns {string|Promise<string>} - The string response.
-   */
   abstract exec(args?: CommandArgs): string | Promise<string>;
 }
